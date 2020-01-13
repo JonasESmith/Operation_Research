83bcf9beb053af50b10184c1e357512b72bc4535
Jonas Smith <pictofoster@gmail.com>
2020-01-13 10:15:18 -0600

    Adding basic file structure and solution

---
+++ Projects/Project_1/ProjectOne.cs
@@ -0,0 +1,15 @@
+﻿/// PROGRAMMER : Jonas Smith
+/// Purpose    : Get inputs from a user and solve a given linear equation.
+/// Resources  : 
+/// 
+
+namespace Project_1
+{
+    class ProjectOne
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
