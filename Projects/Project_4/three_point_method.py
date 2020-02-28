4146d2f48dae3ab113382f23b29b3531fd717faa
oversj96 <45669103+oversj96@users.noreply.github.com>
2020-02-28 12:53:03 -0600

    cleaned up code

--- Projects/Project_4/three_point_method.py
+++ Projects/Project_4/three_point_method.py
@@ -1,40 +1,39 @@
+'''three_point_method.py: Using the three point interval sequential search method, determine
+the local maxima/minima along an initial interval that MUST be unimodal. '''
 
 def function(x):
+    '''Defines the function to be evaluated for.'''
     return x**3 - x + 1
 
-def divide_interval(interval, count=4):
-    distance = interval[1] - interval[0]
+
+def divide_interval(a, b, count=4):
+    '''Calculate the new three point interval given [a, b], returns [x1, x2, x3]'''
+    distance = b - a
     dx = distance / count
-    out = [interval[0] + dx]
+    out = [a + dx]
     for i in range(1, count - 1):
         out.append(out[i - 1] + dx)
     return out
 
 
-
-def three_point_search(func, interval, prob_type, tol=1e-2):
-    if prob_type == "maximize":
-        pass
+def three_point_search(func, a, b, tol=1e-2):
+    '''Implements a system of three point interval search through successive segment elimination.'''
+    f_vals = []
+    three_points = divide_interval(a, b)
+    five_points = three_points.copy()
+    five_points.insert(0, a)
+    five_points.append(b)
+    for i in range(0, len(three_points)):
+        f_vals.append(function(three_points[i]))
+    index_of_min = f_vals.index(min(f_vals)) + 1
+    if (b - a < tol):
+        return five_points[index_of_min]
     else:
-        func_vals = []
-        inner_interval = divide_interval(interval)
-        full_interval = inner_interval.copy()
-        full_interval.insert(0, interval[0])
-        full_interval.append(interval[1])
-        for i in range(0, len(inner_interval)):
-            func_vals.append(function(inner_interval[i]))
-        index_of_min = func_vals.index(min(func_vals)) + 1
-        if (interval[1] - interval[0] < tol):
-            return full_interval[index_of_min]
-        else:
-            new_interval = []
-            new_interval.append(full_interval[index_of_min - 1])
-            new_interval.append(full_interval[index_of_min + 1])
-            return three_point_search(func, new_interval, prob_type)
+        a = five_points[index_of_min - 1]
+        b = five_points[index_of_min + 1]
+        return three_point_search(func, a, b)
     
 
-
-
-
-print(three_point_search(function, [0, 1.28], "minimize"))
+if __name__ == "__main__":
+    print(three_point_search(function, 0, 1.28))
 
