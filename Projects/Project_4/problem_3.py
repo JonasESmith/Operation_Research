1c6368182e63895987428d0c4667db90ae2c107b
oversj96 <45669103+oversj96@users.noreply.github.com>
2020-02-29 18:20:07 -0600

    updated output for prob 3

--- Projects/Project_4/problem_3.py
+++ Projects/Project_4/problem_3.py
@@ -1,19 +1,21 @@
 import sympy
-import numpy as np
-from sympy import symbols, Eq, pprint
-from sympy.solvers.solveset import nonlinsolve, solveset
+from prettytable import PrettyTable
+from sympy import symbols, pprint
+from sympy.solvers.solveset import nonlinsolve
 from sympy.parsing.sympy_parser import parse_expr
-x, y, z, _n = symbols("x y z _n", real=True)
+x, y, z = symbols("x y z", real=True)
 sympy.init_printing(use_unicode=True)
 
 
 def part_derive(expr):
+    '''Get the partial derivatives of the passed expression.'''
     part_dx = expr.diff(x)
     part_dy = expr.diff(y)
     return part_dx, part_dy
 
 
 def critical_points(expr):
+    '''Find the critical points of the expression.'''
     # Take the first order partial derivatives with respect to x and y.
     fx, fy = part_derive(expr)
     system = [fx, fy]
@@ -23,12 +25,16 @@ def critical_points(expr):
     return solved_system
 
 
-def get_maxima_minima(expr, points):
+def get_values(expr, points):
+    '''Plug critical points into the expression and evaluate.'''
+    values = []
     for i in range(0, len(points)):
-        pprint(expr.subs({x: points[i][0], y: points[i][1]}))
+        values.append(expr.subs({x: points[i][0], y: points[i][1]}))
+    return values
 
 
 def convert_to_usable(solved_system):
+    '''This is for navigating the horrors that are the nested arguments of a sympy expression.'''
     usable = []
     for i in range(0, len(solved_system.args)):
         usable.append([
@@ -42,25 +48,44 @@ def classify(expr, points):
     fx, fy = part_derive(expr)
     fxx, fyy = part_derive(fx)[0], part_derive(fy)[1]
     fxy = part_derive(fx)[1]
-    classifications = {}
+    classifications = []
     for i in range(0, len(points)):
         fxx_val = fxx.subs({x:points[i][0], y:points[i][1]})
         fyy_val = fyy.subs({x:points[i][0], y:points[i][1]})
         fxy_val = fxy.subs({x:points[i][0], y:points[i][1]})
         D = (fxx_val * fyy_val) - (fxy_val)**2      
         if D > 0 and fxx_val > 0:
-            classifications[(points[i][0], points[i][1])] = "relative minimum"
+            classifications.append("relative minimum")
         elif D > 0 and fxx_val < 0: 
-            classifications[(points[i][0], points[i][1])] = "relative maximum"
+            classifications.append("relative maximum")
         elif D < 0:
-            classifications[(points[i][0], points[i][1])] = "saddle point"
+            classifications.append("saddle point")
         else:
-            classifications[(points[i][0], points[i][1])] = "no conclusion"
+            classifications.append("no conclusion")
     return classifications
 
     
 if __name__ == "__main__":
     expr = parse_expr("sin(x) + cos(x) + sin(y) - cos(y)", locals())
     points = critical_points(expr)
-    print(classify(expr, points))
-    get_maxima_minima(expr, points)
+    classes = (classify(expr, points))
+    func_values = get_values(expr, points)
+
+    x = PrettyTable()
+    x.field_names = ["Points", "Values", "Classication"]
+    for i in range(0, len(func_values)):
+        x.add_row([f"{points[i]}", f"{func_values[i]}", classes[i]])
+    print(x)
+
+'''
+Output:
+
++------------------+------------+------------------+
+|      Points      |   Values   |   Classication   |
++------------------+------------+------------------+
+| [5*pi/4, 3*pi/4] |     0      |   saddle point   |
+| [5*pi/4, 7*pi/4] | -2*sqrt(2) | relative minimum |
+|  [pi/4, 3*pi/4]  | 2*sqrt(2)  | relative maximum |
+|  [pi/4, 7*pi/4]  |     0      |   saddle point   |
++------------------+------------+------------------+
+'''
