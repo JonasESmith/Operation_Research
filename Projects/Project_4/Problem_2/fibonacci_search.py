7b0af11d485c7c1c3a52e6b1dd909c1618cb23b4
oversj96 <45669103+oversj96@users.noreply.github.com>
2020-03-02 13:19:54 -0600

    fib search output update

--- Projects/Project_4/Problem_2/fibonacci_search.py
+++ Projects/Project_4/Problem_2/fibonacci_search.py
@@ -1,5 +1,6 @@
 import numpy as np 
-
+from prettytable import PrettyTable
+import os
 
 def function(x):
     '''Defines a function to evaluate for given a unimodal interval.
@@ -15,7 +16,7 @@ def fibonacci_numbers(signature, n):
     return signature[:n]
 
 
-def fibonacci_search(a, b, func, epsilon=1e-2):
+def fibonacci_search(a, b, func, epsilon=1e-2, verbose=False):
     '''Searches for a maximum/minimum over a unimodal interval by progressively
     eliminating portions of the interval.'''
     smallest = (b - a) / epsilon
@@ -25,17 +26,41 @@ def fibonacci_search(a, b, func, epsilon=1e-2):
             n_of_smallest = N
             break
     epsil_prime = (b - a) / fib[N] 
-    for N in range(n_of_smallest, 2, -1):   
+    if verbose:
+        itr = 0
+        table = PrettyTable()
+        table.title = f"Minima of x**3 - x + 1 on the Interval [0, 1.28]"
+        table.field_names = ['Iteration', 'a', 'b', 'fib[N]', 'x1', 'x2', 'f(x1)', 'f(x2)', 'Mid-Point ((x1 + x2) / 2)', 'Midpoint Value']
+        working_directory = os.path.dirname(os.path.normpath(__file__))
+        with open(working_directory + "\\fibonacci_search_table.txt", 'w') as file:
+            file.write(f"Smallest Fibonacci Number >= {smallest}: {fib[N]}\n")
+            file.write(f"Tolerance/Epsilon: {epsilon}\n")
+            file.write(f"(b - a) / Fib[N] = ({b} - {a}) / {fib[N]}\n")
+            file.write(f"Epsilon Prime: {epsil_prime}\n\n")    
+        table.add_row([f"{itr}", f"{a:.3f}", f"{b:.3f}", f"{fib[N]}", f"N/A", f"N/A", f"N/A", f"N/A", f"N/A", f"N/A"])   
+    for N in range(n_of_smallest, 2, -1):       
         dx = fib[N-1]*epsil_prime
         x1 = a + dx
         x2 = b - dx
-        fx1, fx2 = func(x1), func(x2)        
+        fx1, fx2 = func(x1), func(x2)                
         if fx1 > fx2:
             a = x2
         else:
             b = x1
-    return ((x1 + x2) / 2), func((x1 + x2) / 2)
+        mid_point, mid_val = ((x1 + x2) / 2), -func((x1 + x2) / 2)
+        if verbose:
+            itr += 1
+            table.add_row([
+                f"{itr}", f"{a:.3f}", f"{b:.3f}", 
+                f"{fib[N]}", f"{x1:.3f}", f"{x2:.3f}", 
+                f"{fx1:.3f}", f"{fx2:.3f}", f"{mid_point:.3f}", 
+                f"{mid_val:.3f}"
+                ])   
+    if verbose:
+        with open(working_directory + "\\fibonacci_search_table.txt", 'a') as file:
+            file.write(str(table))
+    return mid_point, mid_val
     
 
 if __name__ == "__main__":
-    print(fibonacci_search(0, 1.28, function, epsilon=1e-2))
+    print(fibonacci_search(0, 1.28, function, epsilon=1e-2, verbose=True))
