bf2199a8083b2cbd768964f09609b63bbeb68206
oversj96 <45669103+oversj96@users.noreply.github.com>
2020-04-03 17:56:41 -0500

    speed and compression optimizations

--- Projects/Project_6/Project 6.py
+++ Projects/Project_6/Project 6.py
@@ -1,25 +1,26 @@
-
 import os
+import zipfile
+from tkinter import Tk, filedialog, messagebox
+
 import numpy as np
 import pandas as pd
-from tkinter import filedialog, messagebox, Tk
 from prettytable import PrettyTable
 
-#Array of paths.
-#The main diagonal is ignored.
-#All valid paths should be non-negative numbers.
-#Any path that does not exist should be indiated by a negative number (-1).
+# Array of paths.
+# The main diagonal is ignored.
+# All valid paths should be non-negative numbers.
+# Any path that does not exist should be indiated by a negative number (-1).
 
-#Class to store inforation about each node.
+# Class to store inforation about each node.
 class Node:
     index = 0
-    #Is this node starred?
+    # Is this node starred?
     starred = False
-    #Which node did the shortest path come from?
+    # Which node did the shortest path come from?
     nodeFrom = -1
-    #What is the length of the path up to this node?
+    # What is the length of the path up to this node?
     lengthFrom = -1
-    #What branches are available from this node?
+    # What branches are available from this node?
     paths = []
 
     def __init__(self, index):
@@ -33,15 +34,16 @@ class Node:
                 remaining.append(path)
         return remaining
 
-#Class to store information about each path.
+
+# Class to store information about each path.
 class Path:
-    #Index of the starting node
+    # Index of the starting node
     start = 0
-    #Index of the ending node
+    # Index of the ending node
     end = 0
-    #Length of the path
+    # Length of the path
     length = 0
-    #Is this path circled?
+    # Is this path circled?
     circled = False
 
     def __init__(self, start, end, length):
@@ -56,17 +58,16 @@ class Path:
         return self.__str__()
 
 
-#Function to print the node tables
+# Function to print the node tables
 def printNodes():
-    #Find the max number of paths for a single node.
+    # Find the max number of paths for a single node.
     maxPaths = 0
     for node in nodes:
         if len(node.paths) > maxPaths:
             maxPaths = len(node.paths)
 
-
     output = []
-    
+
     output.append(["Node", "Starred", "Total Cost"])
     for i in range(0, maxPaths):
         output[0].append(f"Branch {i}")
@@ -87,7 +88,6 @@ def printNodes():
         output.append(row)
 
     dataframes.append(pd.DataFrame(output))
-    
 
 
 def ask_for_file():
@@ -97,7 +97,7 @@ def ask_for_file():
     # Ask for file with current working directory as starting point
     file = filedialog.askopenfilename(
         initialdir=os.getcwd(),
-        filetypes=([("Excel Sheet", "*.xlsx"), ("Comma Separated Values", "*.csv")]),
+        filetypes=([("Excel Sheet", "*.xlsx")]),
         title="Please Open Your Game's Excel Spreadsheet",
     )
 
@@ -107,96 +107,116 @@ def ask_for_file():
         raise FileNotFoundError("The file specified does not exist! Exiting.")
 
 
+def to_filename(s):
+    return "".join(x if x.isalnum() else "_" for x in s)
+
+
 if __name__ == "__main__":
     try:
         root = Tk()
         root.withdraw()
-        
+
         # Read in data frame from a selected file.
         df = pd.read_excel(ask_for_file(), verbose=True)
         # Little bit of regex magic to find all empty cells and set them to 0.
-        df.replace(r'^\s*$', 0, regex=True)
+        df.replace(r"^\s*$", 0, regex=True)
         # Store column headers to ask the user for source and sink.
         nodeNames = df.columns.tolist()[1:]
         # Drop column headers.
-        df.drop(df.columns[0], axis=1, inplace=True)   
+        df.drop(df.columns[0], axis=1, inplace=True)
         # Convert data frame to useable numpy array.
         paths = np.asarray(df)
         # List data file with indices for user choices.
         for i in range(0, len(nodeNames)):
-            print(f"{i}: {nodeNames[i]}")    
+            print(f"{i}: {nodeNames[i]}")
         # Prompt user for source and sink indices from list.
-        source = int(input("\nFrom the list above, please set the source by its number: "))
-        sink = int(input("\nFrom the list above, please set the sink by its number: " ))
-        
+        source = int(
+            input(
+                "\nFrom the list above, please set the source by its number: "
+            )
+        )
+        sink = int(
+            input(
+                "\nFrom the list above, please set the sink by its number: "
+            )
+        )
+
         print()
         print()
 
         # Make sure the path matrix is square.
         if paths.shape[0] != paths.shape[1]:
             raise ValueError("The path matrix must be square!")
-        
 
-        #Get the number of nodes
+        # Get the number of nodes
         size = paths.shape[0]
 
-        #List to keep track of info about each node.
+        # List to keep track of info about each node.
         nodes = [Node(i) for i in range(0, size)]
         nodes[source].starred = True
         nodes[source].lengthFrom = 0
 
         for row in range(0, size):
             for col in range(0, size):
-                #Add a new path if row is not equal to the column, the value in the matrix is non-zero, the path is not returning to the source, and the row is not the sink.
-                if row != col and paths[row][col] >= 0 and col != source and row != sink:
+                # Add a new path if row is not equal to the column, the value in the matrix is non-zero, the path is not returning to the source, and the row is not the sink.
+                if (
+                    row != col
+                    and paths[row][col] >= 0
+                    and col != source
+                    and row != sink
+                ):
                     nodes[row].paths.append(Path(row, col, paths[row][col]))
 
-        #Sort the paths within each node by length.
+        # Sort the paths within each node by length.
         for node in nodes:
             node.paths.sort(key=lambda path: path.length)
 
         dataframes = []
-
-        #Main program loop. Iterate until we reach the sink.
+        # Main program loop. Iterate until we reach the sink.
         while True:
             printNodes()
-            #Look for the shortest total length after travelling a path from all starred nodes.
-            #Note: The shortest path from a node will always be first in the list of remaining nodes since the lists are sorted and we are removing them as we go.
+            # Look for the shortest total length after travelling a path from all starred nodes.
+            # Note: The shortest path from a node will always be first in the list of remaining nodes since the lists are sorted and we are removing them as we go.
             shortestPath = None
             shortestLength = -1
             for node in nodes:
                 remainingPaths = node.remainingPaths()
-                #Check if the node is starred and has at least one path remaining.
+                # Check if the node is starred and has at least one path remaining.
                 if node.starred and len(remainingPaths) > 0:
-                    #Check if the total length so far plus the length of the next path is less than our current minimum.
-                    #If the shorestLength is -1 we havn't found any node yet, so it will become our current min.
-                    if node.lengthFrom + remainingPaths[0].length < shortestLength or shortestLength == -1:
-                        #Update the shorest path and shorest length.
+                    # Check if the total length so far plus the length of the next path is less than our current minimum.
+                    # If the shorestLength is -1 we havn't found any node yet, so it will become our current min.
+                    if (
+                        node.lengthFrom + remainingPaths[0].length
+                        < shortestLength
+                        or shortestLength == -1
+                    ):
+                        # Update the shorest path and shorest length.
                         shortestPath = remainingPaths[0]
-                        shortestLength = node.lengthFrom + remainingPaths[0].length
-            
-            #Determine the new node based on where the shorest path ends.
+                        shortestLength = (
+                            node.lengthFrom + remainingPaths[0].length
+                        )
+
+            # Determine the new node based on where the shorest path ends.
             newNode = nodes[shortestPath.end]
-            #Circle the new path
+            # Circle the new path
             shortestPath.circled = True
-            #Star the new node, update where it came from, and what the total length to the node is.
+            # Star the new node, update where it came from, and what the total length to the node is.
             newNode.starred = True
             newNode.nodeFrom = shortestPath.start
             newNode.lengthFrom = shortestLength
 
-            #Remove all paths ending at the new node, except for the circled path.
+            # Remove all paths ending at the new node, except for the circled path.
             for node in nodes:
                 for path in node.paths:
                     if path.end == newNode.index and path != shortestPath:
                         del node.paths[node.paths.index(path)]
                         break
-            
 
-            #If the sink node has been starred, break from the loop.
+            # If the sink node has been starred, break from the loop.
             if nodes[sink].starred:
                 break
 
-        #Start at the sink and work backward to find the best path.
+        # Start at the sink and work backward to find the best path.
         outPath = nodeNames[sink]
         curNode = sink
         while True:
@@ -205,21 +225,33 @@ if __name__ == "__main__":
             if curNode == source:
                 break
 
-        #Print the final output
-        print("Path: " + outPath)
-        print(f"Total Length: {nodes[sink].lengthFrom}")
+        # Print the final output
+        output = f"Path: {outPath}\n"
+        output += f"Total Length: {nodes[sink].lengthFrom}"
+        print(output)
 
         print()
         print("Done.")
         print(f"Exporting {len(dataframes)} iterations to output file.")
-        with pd.ExcelWriter('output.xlsx') as writer:
+
+        zip_name = f"{to_filename(nodeNames[source])}_to_{to_filename(nodeNames[sink])}.zip"
+
+        with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zipfile:
+            with open("final_results.txt", 'w') as file:
+                file.write(output)
+            zipfile.write("final_results.txt", "final_results.txt")
+            os.remove("final_results.txt")
             for i in range(0, len(dataframes)):
-                print(f"Exporting iteration {i}")
-                dataframes[i].to_excel(writer, sheet_name=f'Iteration {i}', index_label=False, index=False, header=False)
-            writer.save()
+                print(f'Exporting "Iteration {i}.csv"')
+                dataframes[i].to_csv(f"Iteration_{i}.csv")
+                zipfile.write(f"Iteration_{i}.csv", f"Iteration_{i}.csv")
+                # Since the zipefile class copies a file and does not move
+                # we must tell the os to remove the file manually each iteration.
+                os.remove(f"Iteration_{i}.csv")
+
         print("Done.")
+        print(f"Files exported to '{zip_name}'")
     except FileNotFoundError as fnfe:
         messagebox.showerror("File Not Found", fnfe)
     except ValueError as ve:
         messagebox.showerror("Invalid Input", ve)
-        
